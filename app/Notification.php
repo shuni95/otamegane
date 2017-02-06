5d77c5add43c037910f9df011976c8d4d40fb6a1
Junior Claudio <junior.claudio@tektonlabs.com>
2017-02-06 17:13:59 +0000

    fix previous notification

--- app/Notification.php
+++ app/Notification.php
@@ -8,10 +8,10 @@ class Notification extends Model
 {
     protected $fillable = ['manga', 'chapter', 'title', 'status', 'source_id'];
 
-    public function scopePrevious($manga, $chapter, $source_id)
+    public function scopePrevious($query, $manga, $chapter, $source_id)
     {
-        $notification = Notification::where('manga', $manga)
-                                    ->where('chapter', $chapter)
-                                    ->where('source_id', $source_id);
+        return $query->where('manga', $manga)
+                     ->where('chapter', $chapter)
+                     ->where('source_id', $source_id);
     }
 }
