945ca80e4aeb6c172bbe15c06df162279a66f15c
Junior Claudio <junior.claudio@tektonlabs.com>
2017-02-06 16:48:21 +0000

    sidebar updated

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,6 +1,11 @@
 <div class="ui sidebar vertical tiny labeled icon menu" id="sidebar">
   <a class="item" href="{{ route('dashboard') }}">
-    <i class="dashboard icon"></i>
-    Dashboard
+    <i class="dashboard icon"></i> Dashboard
+  </a>
+  <a class="item" href="{{ route('sources.index') }}">
+    <i class="tag icon"></i> Sources
+  </a>
+  <a class="item" href="{{ route('mangas.index') }}">
+    <i class="book icon"></i> Mangas
   </a>
 </div>
