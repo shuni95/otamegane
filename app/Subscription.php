1dfb1944a83c35a5289d86bd17444c3eb0ae31cd
Junior Claudio <junior.claudio@tektonlabs.com>
2017-02-10 19:55:07 +0000

    fix typo subscription model

--- app/Subscription.php
+++ app/Subscription.php
@@ -10,7 +10,7 @@ class Subscription extends Model
 
     public static function alreadySubscribed($manga_source_id, $chat_id)
     {
-        $subscription = (new static())::where('manga_source_id', $manga_source->id)
+        $subscription = (new static())::where('manga_source_id', $manga_source_id)
             ->where('telegram_chat_id', $chat_id)
             ->first();
 
