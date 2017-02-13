b290722aa3676c41e5528fc416d38d49b25ba3c0
Junior Zavaleta <juniorclaudiozavaleta@gmail.com>
2017-02-13 02:49:38 +0000

    use vue in the index of mangas

--- resources/views/admin/layouts/base.blade.php
+++ resources/views/admin/layouts/base.blade.php
@@ -6,8 +6,13 @@
 
   <title>@yield('title')</title>
   <link rel="stylesheet" type="text/css" href="{{ asset('/semantic.min.css') }}">
+  <style type="text/css">
+    .row-selected { background-color: #E0E1E2; }
+    tr {cursor: pointer;}
+  </style>
   <script src="{{ asset('/jquery-3.1.1.min.js') }}"></script>
   <script src="{{ asset('/semantic.min.js') }}"></script>
+  <script src="{{ asset('/js/vue.min.js') }}"></script>
 
   @stack('head')
 </head>
@@ -23,7 +28,7 @@
   </div>
 
   <div class="pusher">
-    <div class="ui container" style="padding-top: 5em;">
+    <div class="ui container" style="padding-top: 5em;" id="container">
       @yield('content')
     </div>
   </div>
@@ -36,6 +41,10 @@
     .sidebar('toggle');
   });
   </script>
+  <script>
+    window.app_url = "{{ url('/') }}";
+  </script>
+
   @stack('scripts')
 </body>
 </html>
--- resources/views/admin/manga/index.blade.php
+++ resources/views/admin/manga/index.blade.php
@@ -14,32 +14,85 @@
     <a class="ui blue icon button" href="{{ route('mangas.add_form') }}">
       <i class="book icon"></i> Add Manga
     </a>
+
+    <a class="ui green icon button" v-show="mangaSelected" v-bind:href="editUrl">
+      <i class="edit icon"></i> Edit Manga
+    </a>
+
+    <a class="ui gray icon button" v-show="mangaSelected" v-bind:href="showUrl">
+      <i class="eye icon"></i> Show Manga
+    </a>
   </div>
 
-  <table class="ui blue very compact table">
+  <table class="ui blue very hover compact table">
     <thead>
       <th>#</th>
       <th>Name</th>
       <th># subscribers</th>
       <th># subscriptions</th>
       <th># sources</th>
-      <th>Actions</th>
     </thead>
     <tbody>
     @foreach($mangas as $manga)
-      <tr>
+      <tr id="manga-{{ $manga->id }}" v-on:click="selectManga('{{ $manga->id }}')">
         <td>{{ $loop->iteration }}</td>
         <td>{{ $manga->name }}</td>
         <td>{{ $manga->total_subscribers }}</td>
         <td>{{ $manga->total_subscriptions }}</td>
         <td>{{ $manga->num_sources }}</td>
-        <td>
-           <a href="{{ route('mangas.show', ['id' => $manga->id ]) }}" title="Show"><i class="eye icon"></i></a>
-           <a href="{{ route('mangas.edit_form', ['id' => $manga->id ]) }}" title="Edit"><i class="edit icon"></i></a>
-        </td>
       </tr>
     @endforeach
     </tbody>
   </table>
+  <script type="text/javascript">
+    var templateShowUrl = "{{ route('mangas.show',['id' => 'manga_id']) }}";
+    var templateEditUrl = "{{ route('mangas.edit_form',['id' => 'manga_id']) }}";
+  </script>
 
 @endsection
+
+@push('scripts')
+<script>
+var vm = new Vue(
+{
+    el: '#container',
+
+    data: {
+      appUrl: '',
+      mangaSelected: '',
+      showUrl: '',
+      editUrl: '',
+    },
+
+    methods: {
+      selectManga: function(id)
+      {
+        var last_tr = $("#manga-" + this.mangaSelected)
+        var tr_selected = $("#manga-" + id)
+
+        if (this.mangaSelected != id) {
+          last_tr.removeClass("row-selected");
+        }
+
+        tr_selected.toggleClass("row-selected");
+
+        this.mangaSelected = (this.mangaSelected == id) ? 0 : id;
+      },
+    },
+
+    watch: {
+      mangaSelected: function(newValue) {
+        if (newValue > 0) {
+          this.mangaSelected = newValue;
+          this.editUrl = window.templateEditUrl.replace("manga_id", this.mangaSelected);
+          this.showUrl = window.templateShowUrl.replace("manga_id", this.mangaSelected);
+        }
+      }
+    },
+
+    mounted() {
+      this.appUrl = window.app_url;
+    },
+});
+</script>
+@endpush
