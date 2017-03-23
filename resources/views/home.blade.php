4e057520cda0a20fe0dea8957cc14ad54eac2799
Junior Zavaleta <juniorclaudiozavaleta@gmail.com>
2017-03-23 19:36:29 +0000

    use bootstrap default

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,45 +1,31 @@
-@extends('admin.layouts.base')
+@extends('layouts.app')
 
 @section('title', 'Dashboard')
 
 @section('content')
-
-<div class="ui blue segment">
-  <h1>Dashboard</h1>
-</div>
-
-<div class="ui six stackable cards">
-  <div class="blue card">
-    <div class="content">
-      <a class="header" href="{{ route('sources.index') }}" title="Sources">
-        <i class="ui blue huge tag icon"></i>
-      </a>
-    </div>
+<div class="container">
+  <div class="row">
+    <h1>Dashboard</h1>
   </div>
-
-  <div class="blue card">
-    <div class="content">
-      <a class="header" href="{{ route('mangas.index') }}" title="Mangas">
-        <i class="ui huge blue book icon"></i>
-      </a>
-    </div>
+  <div class="row">
+    <a class="btn btn-lg" href="{{ route('sources.index') }}" title="Sources">
+      <span class="glyphicon glyphicon-leaf" aria-hidden="true"></span> Sources
+    </a>
   </div>
-
-  <div class="blue card">
-    <div class="content">
-      <a class="header" href="{{ route('telegram_chats.index') }}" title="Telegram Chats">
-        <i class="send outline huge blue icon"></i>
-      </a>
-    </div>
+  <div class="row">
+    <a class="btn btn-lg" href="{{ route('mangas.index') }}" title="Mangas">
+      <span class="glyphicon glyphicon-book" aria-hidden="true"></span> Mangas
+    </a>
   </div>
-
-  <div class="blue card">
-    <div class="content">
-      <a class="header" href="{{ route('suggestions.index') }}" title="Suggestions">
-        <i class="inbox huge blue icon"></i>
-      </a>
-    </div>
+  <div class="row">
+    <a class="btn btn-lg" href="{{ route('telegram_chats.index') }}" title="Telegram Chats">
+      <span class="glyphicon glyphicon-send" aria-hidden="true"></span> Telegram Chats
+    </a>
+  </div>
+  <div class="row">
+    <a class="btn btn-lg" href="{{ route('suggestions.index') }}" title="Suggestions">
+      <span class="glyphicon glyphicon-inbox" aria-hidden="true"></span> Suggestions
+    </a>
   </div>
 </div>
-
 @endsection
