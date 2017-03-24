f5f099bdfc7bf42f9cc7670b6c9241474384027c
Junior Zavaleta <juniorclaudiozavaleta@gmail.com>
2017-03-24 14:48:23 +0000

    telegram chat test updated

--- tests/Unit/TelegramChatTest.php
+++ tests/Unit/TelegramChatTest.php
@@ -6,9 +6,9 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-use App\TelegramUser;
+use App\TelegramChat;
 
-class TelegramUserTest extends TestCase
+class TelegramChatTest extends TestCase
 {
     use DatabaseMigrations;
 
@@ -17,11 +17,11 @@ class TelegramUserTest extends TestCase
      */
     public function test_create_telegram_user()
     {
-        TelegramUser::create(['user_id' => env('TELEGRAM_TEST_USER_ID', 123456789), 'first_name' => 'Junior', 'last_name' => 'Zavaleta', 'username' => 'JuniorZavaleta']);
+        TelegramChat::create(['chat_id' => env('TELEGRAM_TEST_USER_ID', 123456789), 'first_name' => 'Junior', 'last_name' => 'Zavaleta', 'username' => 'JuniorZavaleta', 'type' => 'private']);
 
-        $this->assertTrue(TelegramUser::all()->count() == 1);
+        $this->assertTrue(TelegramChat::all()->count() == 1);
 
-        $this->assertDatabaseHas('telegram_users' ,[
+        $this->assertDatabaseHas('telegram_chats' ,[
             'username' => 'JuniorZavaleta'
         ]);
     }
